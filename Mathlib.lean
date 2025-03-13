c421722a62cf966daba4e6c9773af8743abe5010
ybenmeur <180416181+ybenmeur@users.noreply.github.com>
2025-03-13 06:53:11 +0000

    feat: definitions for Tannaka duality for finite groups (#22173)
    Introduces the file `Mathlib/RepresentationTheory/Tannaka.lean` with the basic definitions required for the proof of Tannaka duality for finite groups, along with the first lemma showing the injectivity of `equivHom`.
    
    - [x] depends on: #22235
    - [x] depends on: #22668
    
    
    
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4704,6 +4704,7 @@ import Mathlib.RepresentationTheory.Invariants
 import Mathlib.RepresentationTheory.Maschke
 import Mathlib.RepresentationTheory.Rep
 import Mathlib.RepresentationTheory.Submodule
+import Mathlib.RepresentationTheory.Tannaka
 import Mathlib.RingTheory.AdicCompletion.Algebra
 import Mathlib.RingTheory.AdicCompletion.AsTensorProduct
 import Mathlib.RingTheory.AdicCompletion.Basic
